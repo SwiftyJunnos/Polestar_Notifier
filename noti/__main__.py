70cd32af6ac3480eb56254553075243667063e5f
이창준 <crystalashe@naver.com>
2022-10-09 23:16:24 +0900

    옵션 개수로 비교까지 구현

--- noti/__main__.py
+++ noti/__main__.py
@@ -5,6 +5,7 @@ from .secret import (
     TELEGRAM_TOKEN,
     TELEGRAM_ID
 )
+from .polestar_DTO import Polestar
 from .crawl import Crawler
 from .validate import validate_stock
 from .send import TelegramBot
@@ -13,30 +14,80 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 
 import schedule
 import time
+import datetime
+
+SEARCH_DURATION = 5
 
 def main():
     crawler = Crawler(PRECONF_URL)
     driver = crawler.setup(10)
     bot = TelegramBot(TELEGRAM_TOKEN)
-    search_job = schedule.every(5).minutes.do(search_preconf, crawler, driver, bot)
+    updater = Updater(crawler, driver, bot)
 
+    search_job = schedule.every(SEARCH_DURATION).minutes.do(
+        updater.search_preconf,
+        updater.crawler,
+        updater.driver,
+        updater.bot
+    )
+
+    print("알리미 프로그램 시작")
     while True:
         schedule.run_pending()
-        time.sleep(1)
+        time.sleep(SEARCH_DURATION * 60)
 
-def search_preconf(
-    crawler: Crawler,
-    driver: WebDriver,
-    bot: TelegramBot
-):
-    availables = crawler.check_stock(driver)
-    print(f"{availables} cars Availabe now.")
-    if validate_stock(availables):
-        infos = crawler.get_infos(driver, availables)
-        bot.sendImage(TELEGRAM_ID, infos[0]["image"])
-        bot.sendMessage(TELEGRAM_ID, infos[0]["model"])
-    elif not validate_stock(availables):
-        print("No")
+class Updater:
+    availables: list[Polestar] = []
+    num_availables: int = -1
+    is_first_launch: bool = True
+
+    def __init__(
+        self,
+        crawler: Crawler,
+        driver: WebDriver,
+        bot: TelegramBot
+    ):
+        self.crawler = crawler
+        self.driver = driver
+        self.bot = bot
+
+    def compare(self):
+        print(f"{datetime.datetime.now()}: 이전과 옵션 개수가 같습니다.")
+
+    def search_preconf(
+        self,
+        crawler: Crawler,
+        driver: WebDriver,
+        bot: TelegramBot
+    ):
+        currently_available_options = crawler.check_stock(driver)
+        old_availables = self.availables
+
+        if self.is_first_launch:
+            bot.sendMessage(TELEGRAM_ID, f"""
+                폴스타2 검색을 시작합니다.\n현재 검색 주기는 {SEARCH_DURATION}분 입니다.
+            """)
+            self.is_first_launch = False
+
+        # 가능한 목록이 아예 없으면
+        if not validate_stock(currently_available_options):
+            self.availables = []
+            self.num_availables = 0
+            print("현재 구매 가능한 옵션이 없습니다.")
+            return
+        # 개수가 전과 다르면
+        elif self.num_availables != currently_available_options:
+            # 현재 프리컨 목록 전송
+            self.availables = crawler.get_infos(driver, currently_available_options)
+            bot.sendInfo(currently_available_options, self.availables)
+            self.num_availables = currently_available_options
+        else:
+            self.compare()
+            # 개수가 전과 같으면
+            # 비교해본 후 내용이 전과 다르면
+            # 현재 프리컨 목록 전송
+            # 내용이 같으면
+            # 스킵
 
 if __name__ == "__main__":
     main()
--- noti/send.py
+++ noti/send.py
@@ -1,5 +1,7 @@
 import telegram
 
+from noti.polestar_DTO import Polestar
+
 from .secret import (
     TELEGRAM_TOKEN, TELEGRAM_ID
 )
@@ -22,3 +24,23 @@ class TelegramBot:
         message: str
     ):
         self.bot.send_message(chat_id, message)
+
+    def sendInfo(
+        self,
+        num_available: int,
+        availables: list[Polestar]
+    ):
+        self.sendMessage(TELEGRAM_ID, f"현재 구매 가능한 {num_available}개의 옵션이 있습니다.")
+        for available in availables:
+            self.sendImage(TELEGRAM_ID, available["image"])
+            self.sendMessage(TELEGRAM_ID, (
+                f"{available['model']}\n"
+                f"{available['name']}\n\n"
+                f"예상 인도: {available['estimated']}\n"
+                f"출력: {available['power']}\n"
+                f"0➡️100km/h: {available['zero_to_hundred']}\n"
+                f"패키지: {available['packages']}\n"
+                f"1회 충전 주행거리: {available['drive_time_per_charge']}\n"
+                f"{format(available['price'], ',d')}원"
+                )
+            )
